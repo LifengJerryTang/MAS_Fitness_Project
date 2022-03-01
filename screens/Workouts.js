e420eb8a6e7ef092ac2c17c5b7243ba17416bc41
Jerry Tang <jerrytang16@gmail.com>
2022-03-01 12:17:41 -0500

    added scrollview to workout screen

--- screens/Workouts.js
+++ screens/Workouts.js
@@ -1,18 +1,24 @@
 import React, {useEffect, useState} from "react";
-import { VStack, Center, NativeBaseProvider } from "native-base";
+import { VStack, Center, NativeBaseProvider, ScrollView, Text } from "native-base";
 import Card from '../components/ui/Card'
 import {getDataFromDatabase} from "../firebase/FirebaseAPI";
 
 const WorkoutList = (props) => {
-    return (<VStack space={4} alignItems="center">
-                {props.workouts.map((workout) => {
-                    return <Card
-                                imageUrl={workout.imageUrl}
-                                title={workout.name}
-                                description={workout.description}/>
-                })}
-                <Card/>
-            </VStack>);
+    return (
+            <ScrollView mt={75}>
+                <VStack space={4} alignItems="center">
+                    <Text bold fontSize="xl">
+                        Workouts
+                    </Text>
+                    {props.workouts.map((workout) => {
+                        return <Card
+                            imageUrl={workout.imageUrl}
+                            title={workout.name}
+                            description={workout.description}/>
+                    })}
+                </VStack>
+            </ScrollView>
+         );
 }
 
 const Workouts = () => {
