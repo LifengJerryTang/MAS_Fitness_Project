902fe2b1d8ec3180c9a0d5f70cd5a5469b4ea8f0
Jerry Tang <jerrytang16@gmail.com>
2022-03-01 11:08:08 -0500

    first commit

--- screens/Workouts.js
+++ screens/Workouts.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { VStack, Center, NativeBaseProvider } from "native-base";
+
+const WorkoutList = () => {
+    return (<VStack space={4} alignItems="center">
+                <Center w="64" h="20" bg="indigo.300" rounded="md" shadow={3} />
+                <Center w="64" h="20" bg="indigo.500" rounded="md" shadow={3} />
+                <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3} />
+            </VStack>);
+}
 
 const Workouts = () => {
-
+    return (
+        <NativeBaseProvider>
+            <Center flex={1} px="3">
+                <WorkoutList/>
+            </Center>
+        </NativeBaseProvider>
+    );
 }
 
 export default Workouts;
