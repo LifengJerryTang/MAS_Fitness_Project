c399ce126b8798895af677870634a6c76d6a4545
Jerry Tang <jerrytang16@gmail.com>
2022-03-20 21:11:18 -0400

    removed commented code

--- screens/WorkoutHistory/WorkoutHistory.js
+++ screens/WorkoutHistory/WorkoutHistory.js
@@ -1,110 +1,19 @@
 import React, {useEffect, useState} from "react";
 import {
-  Box, HStack, Icon, Text, VStack, StatusBar, Avatar, Image, Input, useColorMode, ScrollView,
-  Pressable, Center, Divider, Button, IconButton, Stack, Link, Hidden, Menu, Container, View, Card,
+  Box, HStack, Icon, Text, VStack, StatusBar, IconButton, Hidden, View, Card,
 } from "native-base";
-import {
-  AntDesign, Entypo, EvilIcons, Feather, FontAwesome, Ionicons,
-} from "@expo/vector-icons";
 
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import {AntDesign} from "@expo/vector-icons";
+
+import {Agenda} from 'react-native-calendars';
 import {NativeBaseProvider} from "native-base/src/core/NativeBaseProvider";
 import {TouchableOpacity} from "react-native";
 import {getCurrUserId, getDataFromDatabase} from "../../firebase/FirebaseAPI";
 
-
-
-const reviews = [
-  {
-    imageUrl:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    name: "Laura Jones",
-    time: "12 May 2021",
-    review:
-      "I loved the quality of their products. Highly recommended to everyone who is looking for comfortable bodysuits for their kids.",
-  },
-  {
-    imageUrl:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    name: "Laura Jones",
-    time: "02 Jan 2021",
-    review:
-      "I loved the quality of their products. Highly recommended to everyone who is looking for comfortable bodysuits for their kids.",
-  },
-  {
-    imageUrl:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    name: "Rati Agarwal",
-    time: "31 Aug 2021",
-    review:
-      "I loved the quality of their products. Highly recommended to everyone who is looking for comfortable bodysuits for their kids.",
-  },
-];
-
-// Might need the following styled button later, so commenting out for right now
-
-// const AddToCartButton = (props) => {
-//   return (
-//     <HStack
-//       mt="5"
-//       space="4"
-//       alignItems="center"
-//       display={{
-//         base: props.base,
-//         md: props.md,
-//       }}
-//     >
-//       <Center
-//         p="2"
-//         borderRadius="4"
-//         _light={{
-//           bg: "primary.100",
-//         }}
-//         _dark={{
-//           bg: "coolGray.900",
-//         }}
-//       >
-//         <Icon
-//           size="8"
-//           name="heart"
-//           as={EvilIcons}
-//           _dark={{
-//             color: "violet.500",
-//           }}
-//           _light={{
-//             color: "primary.900",
-//           }}
-//         />
-//       </Center>
-//       <Button
-//         flex={1}
-//         h="100%"
-//         py={3}
-//         borderRadius="4"
-//         _dark={{
-//           bg: "violet.700",
-//         }}
-//         _light={{
-//           bg: "primary.900",
-//         }}
-//         _text={{
-//           fontSize: "md",
-//           fontWeight: "semibold",
-//         }}
-//       >
-//         Add To Cart
-//       </Button>
-//     </HStack>
-//   );
-// };
-
 export default function WorkoutHistory(props) {
-  // const router = useRouter(); //use incase of Nextjs
-  const [tabName, setTabName] = React.useState("Reviews");
-  const { colorMode } = useColorMode();
+
   const [historyItems, setHistoryItems] = useState({});
 
-  const [items, setItems] = useState({});
 
   useEffect(() => {
    loadHistoryData().then();
